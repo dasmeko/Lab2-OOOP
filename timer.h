7282443edc990a6c71e3566bfba7a3d368e12c25
dasmeko <dashadadaday@gmail.com>
2023-11-29 13:28:28 +0200

    Created a timer header

--- timer.h
+++ timer.h
@@ -1,4 +1,22 @@
 #ifndef TIMER_H
 #define TIMER_H
+#include <QTime>
+#include <QLabel>
+#include <QTime>
 
-#endif // TIMER_H
+
+class Timer {
+    QTime time;
+    QString description;
+    void timerEvent(QTimerEvent *e);
+public:
+    Timer();
+    Timer(QTime time, QLabel description);
+    Timer(QTime time, QString description);
+    QTime getTime();
+    void setTime(QTime newTime);
+    QString getDesc();
+    void setDesc(QString newDesc);
+};
+
+#endif
