03981e6c3e3bcee44677dd2d4a1bcd77f0aad51e
dasmeko <dashadadaday@gmail.com>
2023-11-29 17:48:50 +0200

    Created main window

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,45 @@
 #include "mainwindow.h"
-#include "./ui_mainwindow.h"
+#include <QDebug>
+#include <QScrollBar>
+#include <QPushButton>
+#include <iostream>
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+MainWindow::MainWindow()
 {
-    ui->setupUi(this);
-}
+    doNotLbl = new QLabel("Do not disturbe mode");
+    doNotCheckBox = new QCheckBox();
+    doNotCheckBox->setChecked(false);
+    doNotDisturb = false;
 
-MainWindow::~MainWindow()
-{
-    delete ui;
+    tmp = "\0";
+    temp.setHMS(0,0,0);
+    setToolBar();
+
+    QLabel *tmr = new QLabel("Nearest Timer",this);
+    tmr->setGeometry(50,85,100,20);
+    mainTimerLbl = new QLabel(this);
+    mainTimerLbl->setText("00:00:00");
+    mainTimerLbl->setGeometry(40,100,130,40);
+
+    QFont font = mainTimerLbl->font();
+    font.setPixelSize(32);
+    mainTimerLbl->setFont(font);
+
+    QLabel *desc = new QLabel("Description",this);
+    desc->setGeometry(260,60,70,20);
+
+    mainTimerDescriptionLbl = new QTextEdit(this);
+    mainTimerDescriptionLbl->setGeometry(250,80,230,100);
+    mainTimerDescriptionLbl->setReadOnly(true);
+
+    QLabel *tmrslbl = new QLabel("Timers",this);
+    tmrslbl->setGeometry(40,205,50,20);
+
+    QScrollBar *sb = new QScrollBar(Qt::Vertical);
+
+    listW = new QListWidget(this);
+    listW->setVerticalScrollBar(sb);
+    listW->setGeometry(30,225,200,200);
+
+    startTimer(1000);
 }
